055cb4f1018f15687858729215df1947b398e211
yogesh <ymali2434@gmail.com>
2022-08-23 14:29:47 +0530

    ch

--- hr_movie/app1/views.py
+++ hr_movie/app1/views.py
@@ -4,11 +4,11 @@ import requests
 def GetMovie(request):
     if request.method=='POST':
         movie_name=request.POST["mv"]  
-        if len(movie_name) is not 0:
-            rsp=requests.get(f'https://hrmoviesapi.herokuapp.com/mv/?search={movie_name}').json()
-            return render(request,'show.html',{'mvs':rsp})
-        else:
-            return render(request,'show.html')
+        rsp=requests.get(f'https://hrmoviesapi.herokuapp.com/mv/?search={movie_name}').json()
+        src_mov=requests.get(f'https://hrmoviesapi.herokuapp.com/mv/?').json()
+        return render(request,'show.html',{'mvs':rsp,'atc':src_mov})
+
 
     else:
-        return render(request,'show.html')
+        src_mov=requests.get(f'https://hrmoviesapi.herokuapp.com/mv/?').json()
+        return render(request,'show.html',{'atc':src_mov})
